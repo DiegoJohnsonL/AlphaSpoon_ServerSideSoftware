e904b44ed42c69103ed9242beef09cbd9a287ead
DiegoJohnsonL <diegojohnson46@gmail.com>
2020-10-01 05:17:33 -0500

    Added relation products-restaurant

--- src/main/java/com/gang/alphaspoon/restaurants/domain/model/Product.java
+++ src/main/java/com/gang/alphaspoon/restaurants/domain/model/Product.java
@@ -1,6 +1,9 @@
 package com.gang.alphaspoon.restaurants.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.gang.alphaspoon.model.AuditModel;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -17,6 +20,12 @@ public class Product extends AuditModel {
 
     @NotNull
     private Double price;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)  // Un post puede tener varios comentarios - muchos a uno - Al crear no quiero un comment sin un post(optional = false)
+    @JoinColumn(name = "restaurant_id", nullable = false) // cual va a ser la columna que hara de foreing key en la tabla de comment
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore // en caso se creo un archivo JSON, no agregar la columna post
+    private Restaurant restaurant;
 
     public Product(Long id, String name, Double price) {
         this.id = id;
